063d3640fc4053deaeab4c603aa8e75b7a06a3eb
fzdocente <fzdocente@gmail.com>
2024-10-31 08:35:07 -0500

    Se genera automovil

---
+++ src/Automovil.java
@@ -0,0 +1,11 @@
+public class Automovil extends  vehiculo{
+    private boolean Descapotabloe;
+
+    public boolean isDescapotabloe() {
+        return Descapotabloe;
+    }
+
+    public void setDescapotabloe(boolean descapotabloe) {
+        Descapotabloe = descapotabloe;
+    }
+}
